7c36f2ed5cc28cab67de0534dc34c223421d1c65
Minesh Gandhi <mineshgandhi3262@gmail.com>
2021-01-18 10:10:05 +0530

    Implement LogisticRegression.

---
+++ demodocker/pickelmodel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PickelmodelConfig(AppConfig):
+    name = 'pickelmodel'
---
+++ demodocker/pickelmodel/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+# Create your models here.
+class savemodeltopickel():
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+    
+    names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+    
+    dataframes = pandas.read_csv(url, names=names)
+    arrays = dataframes.values
+    X = arrays[:,0:8]
+    Y = arrays[:,8]
+    
+    test_size = 0.33
+    seed  = 7
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
+    
+    #Fit the model on training set 
+    model = LogisticRegression()
+    model.fit(X_train, Y_train)
+    
+    #save the model to disk
+    filename = 'finalized_model.sav'
+    pickle.dump(model, open(filename, 'wb'))
