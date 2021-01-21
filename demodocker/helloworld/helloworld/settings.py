f73982c1bbe11397bd129cd6542f8ad735db5964
Minesh Gandhi <mineshgandhi3262@gmail.com>
2021-01-22 01:07:12 +0530

    Create DockerFile and Build Docker image.

--- demodocker/helloworld/helloworld/settings.py
+++ demodocker/helloworld/helloworld/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!fez$6d8%!y$&w8!%75&&uk4h&jy804swybqlvnrvxazbty5e4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=1))
 
 ALLOWED_HOSTS = []
 
